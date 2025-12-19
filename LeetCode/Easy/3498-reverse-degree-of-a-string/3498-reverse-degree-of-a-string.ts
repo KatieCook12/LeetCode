8e79145d1c03d857e4f929636ed9afdf476b6408
Katie <97467617+KatieCook12@users.noreply.github.com>
2025-12-19 23:49:30 +0000

    Time: 2 ms (38.78%), Space: 58.5 MB (30.61%) - LeetHub

--- LeetCode/Easy/3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.ts
+++ LeetCode/Easy/3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.ts
@@ -1,9 +1,10 @@
 function reverseDegree(s: string): number {
 
     const sArray: string[] = s.split("");
+    const arrayLength: number = sArray.length;
     let output: number = 0;
 
-    for (let i = 0; i < sArray.length; i++) {
+    for (let i = 0; i < arrayLength; i++) {
         output += (26 - (sArray[i].charCodeAt(0) - 'a'.charCodeAt(0))) * (i + 1);
     }
 

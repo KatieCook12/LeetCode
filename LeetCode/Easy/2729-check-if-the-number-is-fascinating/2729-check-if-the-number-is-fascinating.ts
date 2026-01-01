41a49b40dd9e7b51ebb523c0439c103e64761443
Katie <97467617+KatieCook12@users.noreply.github.com>
2026-01-01 23:28:28 +0000

    Time: 1 ms (76.47%), Space: 57.1 MB (94.12%) - LeetHub

--- LeetCode/Easy/2729-check-if-the-number-is-fascinating/2729-check-if-the-number-is-fascinating.ts
+++ LeetCode/Easy/2729-check-if-the-number-is-fascinating/2729-check-if-the-number-is-fascinating.ts
@@ -1,13 +1,13 @@
 function isFascinating(n: number): boolean {
+    const concat: string = ('' + n + n * 2 + n * 3)
+        .split('')
+        .sort((a, b) => Number(a) - Number(b))
+        .join('');
 
-    const concat: string = (String(n) + String(n * 2) + String(n * 3)).split("").sort((a, b) => Number(a) - Number(b)).join("");
+    if (concat.length !== 9) return false;
 
     for (let i = 1; i <= 9; i++) {
-        if (concat[i - 1] != String(i)) {
-            return false;
-        }
+        if (concat[i - 1] !== String(i)) return false;
     }
-
     return true;
-
-};
+}

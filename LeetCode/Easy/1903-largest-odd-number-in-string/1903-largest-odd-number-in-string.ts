42a88e3f128c5d35f9512a583dca1564a84371f1
Katie <97467617+KatieCook12@users.noreply.github.com>
2026-01-03 10:43:11 +0000

    Time: 1 ms (82.81%), Space: 58.3 MB (95.31%) - LeetHub

--- LeetCode/Easy/1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.ts
+++ LeetCode/Easy/1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.ts
@@ -1,6 +1,8 @@
 function largestOddNumber(num: string): string {
 
-    for (let i = num.length - 1; i >= 0; i--) {
+    const numLength: number = num.length - 1;
+
+    for (let i = numLength; i >= 0; i--) {
         if (Number(num[i]) % 2 != 0) {
             return num.slice(0, i + 1);
         }

6b11753b2256e833bcf9da4f543f26681ec30939
Katie <97467617+KatieCook12@users.noreply.github.com>
2026-01-06 22:17:11 +0000

    Time: 1 ms (79.76%), Space: 57.8 MB (71.43%) - LeetHub

--- LeetCode/Easy/3794-reverse-string-prefix/3794-reverse-string-prefix.ts
+++ LeetCode/Easy/3794-reverse-string-prefix/3794-reverse-string-prefix.ts
@@ -1,9 +1,5 @@
 function reversePrefix(s: string, k: number): string {
 
-    let word: string = "";
-
-    word += s.slice(0, k).split("").reverse().join("").concat(s.slice(k));
-
-    return word;
+    return s.slice(0, k).split("").reverse().join("").concat(s.slice(k));
 
 };

8852682ef66cf4900ac0e29d6c328e08bdbd661f
Katie <97467617+KatieCook12@users.noreply.github.com>
2026-01-07 20:20:06 +0000

    Time: 0 ms (100%), Space: 61.7 MB (82.42%) - LeetHub

---
+++ LeetCode/Easy/0344-reverse-string/0344-reverse-string.ts
@@ -0,0 +1,17 @@
+function reverseString(s: string[]): void {
+
+    let left: number = 0;
+    let right: number = s.length - 1;
+
+    while (left < right) {
+
+        const temp: string = s[left];
+
+        s[left] = s[right];
+        s[right] = temp;
+
+        left++;
+        right--;
+    }
+
+}

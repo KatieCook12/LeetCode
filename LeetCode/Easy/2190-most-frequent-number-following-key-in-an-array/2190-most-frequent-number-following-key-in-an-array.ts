565beac4febfcf3163811e7166d13bea8f31cc4e
Katie <97467617+KatieCook12@users.noreply.github.com>
2026-01-18 13:35:30 +0000

    Time: 3 ms (88.89%), Space: 57.9 MB (66.67%) - LeetHub

---
+++ LeetCode/Easy/2190-most-frequent-number-following-key-in-an-array/2190-most-frequent-number-following-key-in-an-array.ts
@@ -0,0 +1,21 @@
+function mostFrequent(nums: number[], key: number): number {
+
+    const numsLength: number = nums.length;
+    const counts: Record<number, number> = {};
+    let maxCount = 0;
+    let result = nums[0];
+
+    for (let i = 0; i < nums.length - 1; i++) {
+        if (nums[i] === key) {
+            const target = nums[i + 1];
+            counts[target] = (counts[target] ?? 0) + 1;
+
+            if (counts[target] > maxCount) {
+                maxCount = counts[target];
+                result = target;
+            }
+        }
+    }
+
+    return result;
+}

d3b7108ce89dc80f8c1e6cad7d6ab74138d62f7a
Katie <97467617+KatieCook12@users.noreply.github.com>
2026-01-22 19:33:35 +0000

    Time: 5 ms (67.86%), Space: 59.4 MB (60.71%) - LeetHub

---
+++ LeetCode/Easy/2032-two-out-of-three/2032-two-out-of-three.ts
@@ -0,0 +1,28 @@
+function twoOutOfThree(nums1: number[], nums2: number[], nums3: number[]): number[] {
+
+    const nums1Set: Set<number> = new Set(nums1);
+    const nums2Set: Set<number> = new Set(nums2);
+    const nums3Set: Set<number> = new Set(nums3);
+
+    const count: Record<string, number> = {};
+
+    function countSet(set: Set<number>): void {
+        set.forEach((ele: number) => {
+            count[ele] = (count[ele] ?? 0) + 1;
+        });
+    }
+
+    countSet(nums1Set);
+    countSet(nums2Set);
+    countSet(nums3Set);
+
+    const result: number[] = [];
+
+    for (const key in count) {
+        if (count[key] >= 2) {
+            result.push(Number(key));
+        }
+    }
+
+    return result;
+};

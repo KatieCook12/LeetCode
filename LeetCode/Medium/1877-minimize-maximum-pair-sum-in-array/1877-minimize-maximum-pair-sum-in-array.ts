923eed3cd23ec84d65e4d26e9a0123f4ea13cc1c
Katie <97467617+KatieCook12@users.noreply.github.com>
2026-01-24 11:09:50 +0000

    Time: 280 ms (5.56%), Space: 70.1 MB (88.89%) - LeetHub

--- LeetCode/Medium/1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.ts
+++ LeetCode/Medium/1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.ts
@@ -1,17 +1,15 @@
 function minPairSum(nums: number[]): number {
-
     nums.sort((a, b) => a - b);
-    let left: number = 0;
-    let right: number = nums.length - 1;
-    let maxPairSum: number = -Infinity;
 
-    while (left < right) {
+    let left = 0;
+    let right = nums.length - 1;
+    let maxPairSum = nums[left] + nums[right];
+
+    while (++left < --right) {
         const pairSum = nums[left] + nums[right];
         if (pairSum > maxPairSum) {
             maxPairSum = pairSum;
         }
-        left++;
-        right--;
     }
 
     return maxPairSum;

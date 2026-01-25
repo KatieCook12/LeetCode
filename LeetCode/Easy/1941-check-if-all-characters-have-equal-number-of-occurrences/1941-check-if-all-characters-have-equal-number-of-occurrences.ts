535c87ae7b391946c19f23ff43e599084e128068
Katie <97467617+KatieCook12@users.noreply.github.com>
2026-01-25 15:07:31 +0000

    Time: 5 ms (63.29%), Space: 58.4 MB (31.65%) - LeetHub

--- LeetCode/Easy/1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.ts
+++ LeetCode/Easy/1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.ts
@@ -1,15 +1,11 @@
 function areOccurrencesEqual(s: string): boolean {
 
-    const map = new Map();
+    const map = new Map<string, number>();
 
     for (const ch of s) {
-        map.set(ch, (map.get(ch) || 0) + 1);
+        map.set(ch, (map.get(ch) ?? 0) + 1);
     }
 
-    if (new Set(map.values()).size === 1){
-        return true;
-    }
+    return new Set(map.values()).size === 1;
 
-    return false;
-
-};
+}

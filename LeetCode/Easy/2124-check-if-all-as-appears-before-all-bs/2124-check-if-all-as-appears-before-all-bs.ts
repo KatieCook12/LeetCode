7fe2a75f5947161bbbb93ac2f17a05aa1a83ccff
Katie <97467617+KatieCook12@users.noreply.github.com>
2026-01-26 19:47:18 +0000

    Time: 0 ms (100%), Space: 55.6 MB (33.33%) - LeetHub

--- LeetCode/Easy/2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.ts
+++ LeetCode/Easy/2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.ts
@@ -1,15 +1,3 @@
 function checkString(s: string): boolean {
-
-    const sLength: number = s.length;
-
-    for (let i=0; i < sLength; i++){
-
-        if (s[i] === "b" && s[i + 1] === "a"){
-            return false;
-        }
-
-    }
-
-    return true;
-    
-};
+  return !s.includes("ba");
+}

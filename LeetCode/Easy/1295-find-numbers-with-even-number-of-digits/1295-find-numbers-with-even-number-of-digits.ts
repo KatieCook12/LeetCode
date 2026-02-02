0c5a368a93f230aa61a78d0fdbf2815c4fc9d8c7
Katie <97467617+KatieCook12@users.noreply.github.com>
2026-02-02 20:41:08 +0000

    Time: 4 ms (16.48%), Space: 57.6 MB (58.52%) - LeetHub

--- LeetCode/Easy/1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.ts
+++ LeetCode/Easy/1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.ts
@@ -3,11 +3,10 @@ function findNumbers(nums: number[]): number {
     let count: number = 0;
 
     for (const n of nums) {
-        if ((Math.floor(Math.log10(Math.abs(n))) + 1) % 2 === 0) {
+        if (String(n).length % 2 === 0) {
             count++;
         }
     }
 
     return count;
-
 };

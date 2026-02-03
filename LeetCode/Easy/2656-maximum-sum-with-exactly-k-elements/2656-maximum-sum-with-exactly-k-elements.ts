12ad74f1bb04a4f9947128b2f172c2ec5846e3fb
Katie <97467617+KatieCook12@users.noreply.github.com>
2026-02-03 20:38:20 +0000

    Time: 2 ms (70%), Space: 58.4 MB (96%) - LeetHub

--- LeetCode/Easy/2656-maximum-sum-with-exactly-k-elements/2656-maximum-sum-with-exactly-k-elements.ts
+++ LeetCode/Easy/2656-maximum-sum-with-exactly-k-elements/2656-maximum-sum-with-exactly-k-elements.ts
@@ -1,8 +1,6 @@
 function maximizeSum(nums: number[], k: number): number {
 
-    nums.sort((a, b) => a - b)
-
-    let currentNum: number = nums[nums.length - 1]
+    let currentNum: number = Math.max(...nums)
     let count: number = 0;
 
     for (let i = 1; i <= k; i++) {

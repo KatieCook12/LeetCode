1cca8d81be89919f3ba7a7839d36adcf64f2cf25
Katie <97467617+KatieCook12@users.noreply.github.com>
2026-02-13 00:00:18 +0000

    Time: 1 ms (52.38%), Space: 56.5 MB (14.29%) - LeetHub

--- LeetCode/Easy/1844-replace-all-digits-with-characters/1844-replace-all-digits-with-characters.ts
+++ LeetCode/Easy/1844-replace-all-digits-with-characters/1844-replace-all-digits-with-characters.ts
@@ -2,11 +2,12 @@ function replaceDigits(s: string): string {
 
     const replacedDigits: string[] = [];
     const sLength: number = s.length;
+    const alphabet: string = "abcdefghijklmnopqrstuvwxyz"
 
     for (let i = 0; i < sLength; i++) {
 
         if (s[i].charCodeAt(0) <= 57) {
-            replacedDigits.push(String.fromCharCode(s[i - 1].charCodeAt(0) + Number(s[i])))
+            replacedDigits.push(alphabet[alphabet.indexOf(s[i - 1]) + Number(s[i])])
         }
 
         else {

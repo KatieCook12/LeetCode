95e0436fe0792503ba79790fc47af673a8a029d7
Katie <97467617+KatieCook12@users.noreply.github.com>
2026-02-25 10:32:06 +0000

    Time: 5 ms (60.58%), Space: 56.6 MB (74.09%) - LeetHub

--- LeetCode/Easy/0389-find-the-difference/0389-find-the-difference.ts
+++ LeetCode/Easy/0389-find-the-difference/0389-find-the-difference.ts
@@ -2,12 +2,12 @@ function findTheDifference(s: string, t: string): string {
 
     let findDifference: number = 0;
 
-    for (let char of s) {
-        findDifference ^= char.charCodeAt(0);
+    for (const ch of s) {
+        findDifference ^= ch.charCodeAt(0);
     }
 
-    for (let char of t) {
-        findDifference ^= char.charCodeAt(0);
+    for (const ch of t) {
+        findDifference ^= ch.charCodeAt(0);
     }
 
     return String.fromCharCode(findDifference);

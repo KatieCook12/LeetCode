7c4ed31e2de6382969bd7ea26e26ec77e0aabcfe
Katie <97467617+KatieCook12@users.noreply.github.com>
2026-03-01 11:10:47 +0000

    Time: 3 ms (37.5%), Space: 59.1 MB (43.75%) - LeetHub

---
+++ LeetCode/Easy/2194-cells-in-a-range-on-an-excel-sheet/2194-cells-in-a-range-on-an-excel-sheet.ts
@@ -0,0 +1,15 @@
+function cellsInRange(s: string): string[] {
+
+    const cellsInRangeArray: string[] = [];
+
+    for (let i = s[0].charCodeAt(0); i <= s[3].charCodeAt(0); i++) {
+
+        for (let x = +s[1]; x <= +s[4]; x++) {
+            cellsInRangeArray.push(String.fromCharCode(i) + String(x));
+        }
+
+    }
+
+    return cellsInRangeArray;
+
+};

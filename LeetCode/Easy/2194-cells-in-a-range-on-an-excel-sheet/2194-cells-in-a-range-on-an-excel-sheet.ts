d28d937922878af1554d239dfcd067b1450426e5
Katie <97467617+KatieCook12@users.noreply.github.com>
2026-03-01 11:13:22 +0000

    Time: 5 ms (18.75%), Space: 59.8 MB (6.25%) - LeetHub

--- LeetCode/Easy/2194-cells-in-a-range-on-an-excel-sheet/2194-cells-in-a-range-on-an-excel-sheet.ts
+++ LeetCode/Easy/2194-cells-in-a-range-on-an-excel-sheet/2194-cells-in-a-range-on-an-excel-sheet.ts
@@ -5,7 +5,7 @@ function cellsInRange(s: string): string[] {
     for (let i = s[0].charCodeAt(0); i <= s[3].charCodeAt(0); i++) {
 
         for (let x = +s[1]; x <= +s[4]; x++) {
-            cellsInRangeArray.push(String.fromCharCode(i) + String(x));
+            cellsInRangeArray.push(String.fromCharCode(i) + x);
         }
 
     }

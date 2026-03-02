9db6aa64b8cd26681918788c8ea577d2870d45af
Katie <97467617+KatieCook12@users.noreply.github.com>
2026-03-02 22:53:28 +0000

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/2710-remove-trailing-zeros-from-a-string/2710-remove-trailing-zeros-from-a-string.ts
+++ LeetCode/Easy/2710-remove-trailing-zeros-from-a-string/2710-remove-trailing-zeros-from-a-string.ts
@@ -1,15 +1,5 @@
 function removeTrailingZeros(num: string): string {
 
-    const numArray: string[] = num.split("");
-    const numArrayLength: number = numArray.length - 1;
-    let index: number = 0;
-
-    for (let i = numArrayLength; i >= 0; i--) {
-        if (numArray[i] != "0") {
-            index = i + 1;
-            break;
-        }
-    }
-
-    return numArray.slice(0, index).join("");
+    return num.replace(/0+$/, "");
+    
 };

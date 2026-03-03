f127bb00ed3f41fd47bc2108833daf18e463ff0f
Katie <97467617+KatieCook12@users.noreply.github.com>
2026-03-03 13:56:02 +0000

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/0521-longest-uncommon-subsequence-i/0521-longest-uncommon-subsequence-i.ts
+++ LeetCode/Easy/0521-longest-uncommon-subsequence-i/0521-longest-uncommon-subsequence-i.ts
@@ -1,10 +1,10 @@
 function findLUSlength(a: string, b: string): number {
 
-    if ( a == b){
+    if (a == b) {
         return -1;
     }
     else {
         return a.length > b.length ? a.length : b.length;
     }
-    
+
 };

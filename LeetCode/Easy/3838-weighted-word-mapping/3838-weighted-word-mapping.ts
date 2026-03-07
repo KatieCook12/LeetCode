46bafa5be7858760218b6a5fb8f16f12f1a3608d
Katie <97467617+KatieCook12@users.noreply.github.com>
2026-03-07 09:14:15 +0000

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/3838-weighted-word-mapping/3838-weighted-word-mapping.ts
+++ LeetCode/Easy/3838-weighted-word-mapping/3838-weighted-word-mapping.ts
@@ -1,14 +1,14 @@
 function mapWordWeights(words: string[], weights: number[]): string {
 
-    const result: string[] = [];
+    let result: string = "";
 
     for (const word of words) {
         let sum: number = 0;
         for (const ch of word) {
             sum += weights[ch.charCodeAt(0) - 97];
         }
-        result.push(String.fromCharCode(26 - (sum % 26) + 96));
+        result += String.fromCharCode(26 - (sum % 26) + 96);
     }
 
-    return result.join("");
+    return result;
 };

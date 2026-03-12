943001d3fd53115e9e77711d465dadf0bb299d25
Katie <97467617+KatieCook12@users.noreply.github.com>
2026-03-12 10:16:01 +0000

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.ts
+++ LeetCode/Easy/0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.ts
@@ -1,16 +1,22 @@
 function countPrimeSetBits(left: number, right: number): number {
 
-    const primeNumbers: number[] = [2, 3, 5, 7, 11, 13, 17, 19];
-    let count: number = 0;
+    const primes = [2, 3, 5, 7, 11, 13, 17, 19];
+    let count = 0;
 
-    for (let i = left; i <= right; i++) {
+    for (let n = left; n <= right; n++) {
 
-        if (primeNumbers.includes(i.toString(2).replace(/0/g, "").length)) {
-            count++;
+        let num = n;
+        let bits = 0;
+
+        while (num) {
+            num = num & (num - 1);
+            bits++;
         }
 
+        if (primes.includes(bits)) {
+            count++;
+        }
     }
 
     return count;
-
-};
+}

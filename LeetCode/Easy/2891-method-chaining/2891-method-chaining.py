3000ad6a8732feaebaa02456de9463cf4f792a2d
Katie <97467617+KatieCook12@users.noreply.github.com>
2026-03-15 11:41:29 +0000

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/2891-method-chaining/2891-method-chaining.py
+++ LeetCode/Easy/2891-method-chaining/2891-method-chaining.py
@@ -1,9 +1,4 @@
 import pandas as pd
 
 def findHeavyAnimals(animals: pd.DataFrame) -> pd.DataFrame:
-    df = pd.DataFrame(animals)
-    df = df[df['weight'] > 100]
-    df = df.sort_values(by='weight', ascending=False)
-    return df[['name']]
-
-    
+    return animals[animals['weight'] > 100].sort_values(by='weight', ascending=False)[['name']]

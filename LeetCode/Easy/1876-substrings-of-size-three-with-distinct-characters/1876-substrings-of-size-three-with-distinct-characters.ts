c088b498416a3dd9246bc0701a6a6a1c6bc35822
Katie <97467617+KatieCook12@users.noreply.github.com>
2026-03-20 11:09:03 +0000

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.ts
+++ LeetCode/Easy/1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.ts
@@ -1,7 +1,7 @@
 function countGoodSubstrings(s: string): number {
 
     let count: number = 0;
-    let sLength: number = s.length - 2;
+    const sLength: number = s.length - 2;
 
     for (let i = 0; i < sLength; i++) {
         if (s[i] !== s[i + 1] &&

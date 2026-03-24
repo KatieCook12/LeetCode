66fc3ce3afb0cbcf336052d834d423b887b8fa01
Katie <97467617+KatieCook12@users.noreply.github.com>
2026-03-24 11:24:45 +0000

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.ts
+++ LeetCode/Easy/1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.ts
@@ -1,13 +1,14 @@
 function finalPrices(prices: number[]): number[] {
-    const discountedPrices = [...prices];
+    const discountedPrices: number[] = [...prices];
     const pricesLength: number = prices.length;
 
     for (let i = 0; i < pricesLength; i++) {
-        const discount: number = prices
-            .slice(i + 1)
-            .find(price => price <= prices[i]);
-
-        discountedPrices[i] = prices[i] - (discount ?? 0);
+        for (let j = i + 1; j < pricesLength; j++) {
+            if (prices[j] <= prices[i]) {
+                discountedPrices[i] = prices[i] - prices[j];
+                break;
+            }
+        }
     }
 
     return discountedPrices;

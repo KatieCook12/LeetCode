e396c0f7fa69b19ec93be20c66d769824c694413
Katie <97467617+KatieCook12@users.noreply.github.com>
2026-03-27 10:58:07 +0000

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.ts
+++ LeetCode/Easy/2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.ts
@@ -2,11 +2,13 @@ function countElements(nums: number[]): number {
 
     let count: number = 0;
 
+    const min: number = Math.min(...nums);
+    const max: number = Math.max(...nums);
+
     for (const num of nums) {
-        if (num == Math.min(...nums) || num == Math.max(...nums)) {
-            count--;
+        if (num !== min && num !== max) {
+            count++;
         }
-        count++;
     }
 
     return count;

de7087c2ca5c17564b34b77f663d979e21c6ced4
Katie <97467617+KatieCook12@users.noreply.github.com>
2026-04-01 09:58:25 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.ts
+++ LeetCode/Easy/1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.ts
@@ -2,7 +2,8 @@ function removeDuplicates(s: string): string {
     const stack: string[] = [];
 
     for (const ch of s) {
-        stack.length > 0 && stack[stack.length - 1] === ch ? stack.pop() : stack.push(ch);
+        const stackLength: number = stack.length;
+        stackLength > 0 && stack[stackLength - 1] === ch ? stack.pop() : stack.push(ch);
     }
 
     return stack.join("");

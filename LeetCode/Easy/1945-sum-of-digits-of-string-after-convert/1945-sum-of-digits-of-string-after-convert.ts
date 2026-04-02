23e0f7ad7eef14c2b7707e13eb0c8dc8833f8d3b
Katie <97467617+KatieCook12@users.noreply.github.com>
2026-04-02 14:57:48 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.ts
+++ LeetCode/Easy/1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.ts
@@ -1,17 +1,20 @@
 function getLucky(s: string, k: number): number {
 
-    let currentNumber: number[] = [];
+    let value: number = 0;
 
-    for (const ch of s){
-        (ch.charCodeAt(0) - 96).toString().split("").forEach(num => currentNumber.push(+num));
+    for (const ch of s) {
+        const n = ch.charCodeAt(0) - 96;
+        value += Math.floor(n / 10) + (n % 10);
     }
 
-    while ( k > 0){
-        let currentValue = 0;
-        currentNumber.forEach(num => currentValue += num)
-        currentNumber = currentValue.toString().split("").map(Number)
-        k--;
+    while (--k > 0) {
+        let next: number = 0;
+        while (value > 0) {
+            next += value % 10;
+            value = Math.floor(value / 10);
+        }
+        value = next;
     }
 
-    return +currentNumber.join("")
-};
+    return value;
+}

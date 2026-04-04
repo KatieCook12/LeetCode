8c08e0f5408b47ff39386ddecba89678f06ff560
Katie <97467617+KatieCook12@users.noreply.github.com>
2026-04-04 18:49:39 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/1450-number-of-students-doing-homework-at-a-given-time/1450-number-of-students-doing-homework-at-a-given-time.ts
+++ LeetCode/Easy/1450-number-of-students-doing-homework-at-a-given-time/1450-number-of-students-doing-homework-at-a-given-time.ts
@@ -1,8 +1,9 @@
 function busyStudent(startTime: number[], endTime: number[], queryTime: number): number {
 
     let count: number = 0;
+    const startTimeLength = startTime.length;
 
-    for (let i = 0; i < startTime.length; i++) {
+    for (let i = 0; i < startTimeLength; i++) {
         if (startTime[i] <= queryTime) {
             if (endTime[i] >= queryTime) {
                 count++;

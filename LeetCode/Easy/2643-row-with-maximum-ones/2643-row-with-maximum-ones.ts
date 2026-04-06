cd9ad47c86b723270f5454e790a047db092bda72
Katie <97467617+KatieCook12@users.noreply.github.com>
2026-04-06 19:15:06 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/2643-row-with-maximum-ones/2643-row-with-maximum-ones.ts
+++ LeetCode/Easy/2643-row-with-maximum-ones/2643-row-with-maximum-ones.ts
@@ -1,17 +1,13 @@
 function rowAndMaximumOnes(mat: number[][]): number[] {
+    let index = 0;
+    let count = 0;
 
-    let index: number = 0;
-    let count: number = 0;
-    const matLength: number = mat.length;
+    for (let i = 0; i < mat.length; i++) {
+        let currentCount = 0;
 
-    for (let i = 0; i < matLength; i++) {
-        let currentCount: number = 0;
-
-        mat[i].forEach(num => {
-            if (num == 1) {
-                currentCount++;
-            }
-        });
+        for (const num of mat[i]) {
+            if (num === 1) currentCount++;
+        }
 
         if (currentCount > count) {
             count = currentCount;
@@ -20,4 +16,4 @@ function rowAndMaximumOnes(mat: number[][]): number[] {
     }
 
     return [index, count];
-};
+}

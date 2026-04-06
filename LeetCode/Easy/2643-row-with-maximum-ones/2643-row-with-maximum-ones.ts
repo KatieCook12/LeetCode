c852f6ca0f89a95514734b96b939fdf208d7de9d
Katie <97467617+KatieCook12@users.noreply.github.com>
2026-04-06 19:13:06 +0100

    Time: 7 ms (16.67%), Space: 63.7 MB (63.89%) - LeetHub

---
+++ LeetCode/Easy/2643-row-with-maximum-ones/2643-row-with-maximum-ones.ts
@@ -0,0 +1,23 @@
+function rowAndMaximumOnes(mat: number[][]): number[] {
+
+    let index: number = 0;
+    let count: number = 0;
+    const matLength: number = mat.length;
+
+    for (let i = 0; i < matLength; i++) {
+        let currentCount: number = 0;
+
+        mat[i].forEach(num => {
+            if (num === 1) {
+                currentCount++;
+            }
+        });
+
+        if (currentCount > count) {
+            count = currentCount;
+            index = i;
+        }
+    }
+
+    return [index, count];
+};

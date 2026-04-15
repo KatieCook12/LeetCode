46895489525125c23e0350ddf166e8eb54315d1c
Katie <97467617+KatieCook12@users.noreply.github.com>
2026-04-15 22:53:43 +0100

    Time: 1 ms (64.71%), Space: 58.3 MB (58.82%) - LeetHub

---
+++ LeetCode/Easy/3750-minimum-number-of-flips-to-reverse-binary-string/3750-minimum-number-of-flips-to-reverse-binary-string.ts
@@ -0,0 +1,18 @@
+function minimumFlips(n: number): number {
+
+    const toBinary: string = n.toString(2);
+    let count: number = 0;
+
+    let left: number = 0;
+    let right: number = toBinary.length - 1;
+
+    while (left < right){
+        if (toBinary[left] != toBinary[right]){
+            count += 2;
+        }
+        left ++;
+        right --;
+    }
+    return count;
+
+};

ac94e8d4ffa9273cbb3e28cdedfd9e493cb215c5
Katie <97467617+KatieCook12@users.noreply.github.com>
2026-04-16 22:37:22 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/3274-check-if-two-chessboard-squares-have-the-same-color/3274-check-if-two-chessboard-squares-have-the-same-color.ts
+++ LeetCode/Easy/3274-check-if-two-chessboard-squares-have-the-same-color/3274-check-if-two-chessboard-squares-have-the-same-color.ts
@@ -1,19 +1,9 @@
 function checkTwoChessboards(coordinate1: string, coordinate2: string): boolean {
 
     function colourChecker(coordinate: string): boolean {
+        return coordinate[0].charCodeAt(0) + +coordinate[1]) % 2 == 0 ? true : false;
 
-        
-        if ((coordinate[0].charCodeAt(0) + +coordinate[1]) % 2 == 0) {
-            return true;
-        }
-        else {
-            return false;
-        }
     }
 
-    if (colourChecker(coordinate1) == colourChecker(coordinate2)) {
-        return true;
-    } else {
-        return false;
-    }
+    return colourChecker(coordinate1) == colourChecker(coordinate2) ? true : false;
 };

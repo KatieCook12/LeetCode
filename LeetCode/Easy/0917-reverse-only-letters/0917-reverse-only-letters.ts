3b31eb86c535fbcd80b9533fb3749341f8bc95d3
Katie <97467617+KatieCook12@users.noreply.github.com>
2026-04-19 00:38:11 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/0917-reverse-only-letters/0917-reverse-only-letters.ts
+++ LeetCode/Easy/0917-reverse-only-letters/0917-reverse-only-letters.ts
@@ -2,10 +2,11 @@ function reverseOnlyLetters(s: string): string {
 
     const reversedOnlyLetters: string[] = [];
     let index: number = 0;
+    const sLength: number = s.length;
 
     const reversedLetters = s.replace(/[^a-zA-Z]/g, "").split("").reverse();
 
-    for (let i = 0; i < s.length; i++) {
+    for (let i = 0; i < sLength; i++) {
         const code = s[i].charCodeAt(0);
         if ((code >= 65 && code <= 90) || (code >= 97 && code <= 122)) {
             reversedOnlyLetters.push(reversedLetters[index]);

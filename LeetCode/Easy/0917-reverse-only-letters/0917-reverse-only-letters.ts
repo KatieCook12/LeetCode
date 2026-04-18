1d30b33af7a41a3696559e61a575e5e3ceb8473c
Katie <97467617+KatieCook12@users.noreply.github.com>
2026-04-19 00:32:52 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/0917-reverse-only-letters/0917-reverse-only-letters.ts
+++ LeetCode/Easy/0917-reverse-only-letters/0917-reverse-only-letters.ts
@@ -1,35 +1,26 @@
 function reverseOnlyLetters(s: string): string {
 
     const reversedLetters: string[] = [];
-    let reversedOnlyLetters: string[] = [];
-    let counter: number = 0;
-
-    const isLetter = (char) => {
-        const code = char.charCodeAt(0);
-        return (
-            (code >= 65 && code <= 90) || // A-Z
-            (code >= 97 && code <= 122)   // a-z
-        );
-    };
+    const reversedOnlyLetters: string[] = [];
+    let index = 0;
 
     for (let i = s.length - 1; i >= 0; i--) {
-        if (isLetter(s[i])) {
+        const code = s[i].charCodeAt(0);
+        if ((code >= 65 && code <= 90) || (code >= 97 && code <= 122)) {
             reversedLetters.push(s[i]);
         }
     }
 
     for (let i = 0; i < s.length; i++) {
-        if (!isLetter(s[i])) {
-            reversedOnlyLetters.push(s[i])
-        }
-        else {
-            reversedOnlyLetters.push(reversedLetters[counter])
-            counter++
+        const code = s[i].charCodeAt(0);
+        if ((code >= 65 && code <= 90) || (code >= 97 && code <= 122)) {
+            reversedOnlyLetters.push(reversedLetters[index]);
+            index++;
+        } else {
+            reversedOnlyLetters.push(s[i]);
         }
     }
 
     return reversedOnlyLetters.join("");
 
-
-
-};
+}

571f7068cabc989296fcc4fa5f557ac5b802e026
Katie <97467617+KatieCook12@users.noreply.github.com>
2026-04-20 10:12:04 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/2103-rings-and-rods/2103-rings-and-rods.ts
+++ LeetCode/Easy/2103-rings-and-rods/2103-rings-and-rods.ts
@@ -24,8 +24,8 @@ function countPoints(rings: string): number {
     }
 
     for (const pole in poleColours) {
-        const colours = poleColours[pole];
 
+        const colours = poleColours[pole];
         if (colours.Red && colours.Green && colours.Blue) {
             count++;
         }
@@ -33,8 +33,4 @@ function countPoints(rings: string): number {
     }
 
     return count;
-
-
-
-
 };

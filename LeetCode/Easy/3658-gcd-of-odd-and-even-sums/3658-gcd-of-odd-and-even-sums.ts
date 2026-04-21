3b8c8f6fdb2d6684ac75a607c70bea2b0019e077
Katie <97467617+KatieCook12@users.noreply.github.com>
2026-04-21 23:36:50 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.ts
+++ LeetCode/Easy/3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.ts
@@ -1,11 +1,4 @@
 function gcdOfOddEvenSums(n: number): number {
 
-    let sumOdd: number = 0;
-    let sumEven: number = 0;
-
-    for (let i = 1; i < n * 2 + 1; i++) {
-        i % 2 != 0 ? sumOdd += i : sumEven += i;
-    }
-
-    return Math.abs(sumOdd - sumEven);
+    return n;
 };

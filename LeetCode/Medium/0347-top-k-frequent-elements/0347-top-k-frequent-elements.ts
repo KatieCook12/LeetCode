84cb07ab6ebccd134cb2e53a6c41860d4fc0bf85
Katie <97467617+KatieCook12@users.noreply.github.com>
2026-04-24 20:37:42 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/0347-top-k-frequent-elements/0347-top-k-frequent-elements.ts
+++ LeetCode/Medium/0347-top-k-frequent-elements/0347-top-k-frequent-elements.ts
@@ -1,10 +1,12 @@
 function topKFrequent(nums: number[], k: number): number[] {
-
-    let count: Record<string, number> = {};
+    const count: Record<string, number> = {};
 
     for (const num of nums) {
         count[num] = (count[num] ?? 0) + 1;
     }
 
-    return Object.entries(count).sort((a, b) => b[1] - a[1]).map(([word]) => Number(word)).slice(0, k);
-};
+    return Object.entries(count)
+        .sort((a, b) => b[1] - a[1])
+        .slice(0, k)
+        .map(([num]) => Number(num));
+}

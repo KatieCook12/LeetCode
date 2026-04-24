bb20f4feb76cd8cc4d847a804f68eee0caf51e87
Katie <97467617+KatieCook12@users.noreply.github.com>
2026-04-24 12:50:46 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.ts
+++ LeetCode/Easy/0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.ts
@@ -2,11 +2,7 @@ function uncommonFromSentences(s1: string, s2: string): string[] {
 
     let count: Record<string, number> = {};
 
-    for (const word of s1.split(" ")) {
-        count[word] = (count[word] ?? 0) + 1;
-    }
-
-    for (const word of s2.split(" ")) {
+    for (const word of s1.split(" ") + s2.split(" ")) {
         count[word] = (count[word] ?? 0) + 1;
     }
 

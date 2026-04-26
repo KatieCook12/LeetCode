905d2f9ded82e3223746edc41ba23daec4ca1a56
Katie <97467617+KatieCook12@users.noreply.github.com>
2026-04-26 22:27:50 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.ts
+++ LeetCode/Easy/1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.ts
@@ -1,7 +1,6 @@
 function frequencySort(nums: number[]): number[] {
 
     const frequency: Record<string, number> = {};
-    const sortedArray: number[] = [];
 
     for (const num of nums) {
         frequency[num] = (frequency[num] ?? 0) + 1;
@@ -14,11 +13,5 @@ function frequencySort(nums: number[]): number[] {
         return Number(b[0]) - Number(a[0]);
     });
 
-
-    const finalArray = sorted.flatMap(([digit, freq]) =>
-        Array(freq).fill(Number(digit))
-    );
-
-    return finalArray;
-
+    return sorted.flatMap(([digit, freq]) => Array(freq).fill(Number(digit));
 };

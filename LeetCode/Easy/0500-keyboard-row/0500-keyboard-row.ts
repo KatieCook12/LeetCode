7141d7133f327d7e7d514c679a677b63b7aeafb7
Katie <97467617+KatieCook12@users.noreply.github.com>
2026-05-01 13:42:56 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/0500-keyboard-row/0500-keyboard-row.ts
+++ LeetCode/Easy/0500-keyboard-row/0500-keyboard-row.ts
@@ -1,12 +1,8 @@
 function findWords(words: string[]): string[] {
 
-    const firstRowRegex = /^[qwertyuiopQWERTYYUIOP]+$/i;
-    const secondRowRegex = /^[asdfghjklASDFGHJKL]+$/i;
-    const thirdRowRegex = /^[zxcvbnmZXCVBNM]+$/i;
-
-    const resultFirstRow = words.filter(word => firstRowRegex.test(word));
-    const resultSecondRow = words.filter(word => secondRowRegex.test(word));
-    const resultThirdRow = words.filter(word => thirdRowRegex.test(word));
+    const resultFirstRow = words.filter(word => /^[qwertyuiopQWERTYYUIOP]+$/i.test(word));
+    const resultSecondRow = words.filter(word => /^[asdfghjklASDFGHJKL]+$/i.test(word));
+    const resultThirdRow = words.filter(word => /^[zxcvbnmZXCVBNM]+$/i.test(word));
 
     return [...resultFirstRow, ...resultSecondRow, ...resultThirdRow];
 

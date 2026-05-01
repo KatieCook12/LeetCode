ddffa693ff89eb0276358738aafde48f411fef6d
Katie <97467617+KatieCook12@users.noreply.github.com>
2026-05-01 13:43:35 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/0500-keyboard-row/0500-keyboard-row.ts
+++ LeetCode/Easy/0500-keyboard-row/0500-keyboard-row.ts
@@ -1,10 +1,8 @@
 function findWords(words: string[]): string[] {
 
-    const resultFirstRow = words.filter(word => /^[qwertyuiopQWERTYYUIOP]+$/i.test(word));
-    const resultSecondRow = words.filter(word => /^[asdfghjklASDFGHJKL]+$/i.test(word));
-    const resultThirdRow = words.filter(word => /^[zxcvbnmZXCVBNM]+$/i.test(word));
 
-    return [...resultFirstRow, ...resultSecondRow, ...resultThirdRow];
+    return [...words.filter(word => /^[qwertyuiopQWERTYYUIOP]+$/i.test(word)), 
+    ...words.filter(word => /^[asdfghjklASDFGHJKL]+$/i.test(word)), ...words.filter(word => /^[zxcvbnmZXCVBNM]+$/i.test(word))];
 
 
 };

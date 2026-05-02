338bce4a63ff4255e17a6c9c8b50dd12d3a49387
Katie <97467617+KatieCook12@users.noreply.github.com>
2026-05-02 19:25:30 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.ts
+++ LeetCode/Easy/2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.ts
@@ -1,7 +1,8 @@
 function findDifference(nums1: number[], nums2: number[]): number[][] {
 
-    const differenceNumsOne = [...new Set(nums1.filter(num => !nums2.includes(num)))];
-    const differenceNumsTwo = [...new Set(nums2.filter(num => !nums1.includes(num)))];
+    return [
+        [...new Set(nums1.filter(num => !nums2.includes(num)))],
+        [...new Set(nums2.filter(num => !nums1.includes(num)))]
+    ]
 
-    return [differenceNumsOne, differenceNumsTwo]
 };

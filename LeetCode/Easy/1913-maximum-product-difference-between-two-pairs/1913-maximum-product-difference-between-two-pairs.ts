d327d010a312c1e73790ba7c2ffe5c64e9f4eef6
Katie <97467617+KatieCook12@users.noreply.github.com>
2026-05-05 10:48:54 +0100

    Time: 75 ms (12.73%), Space: 59.1 MB (16.36%) - LeetHub

---
+++ LeetCode/Easy/1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.ts
@@ -0,0 +1,16 @@
+function maxProductDifference(nums: number[]): number {
+
+    const sorted: number[] = nums.sort((a, b) => a - b);
+
+    const largestNumberSum = sorted.slice(nums.length - 2, nums.length).reduce(
+        (accumulator, num) => accumulator *= num,
+        1,
+    );
+
+    const smallestNumberSum = sorted.slice(0, 2).reduce(
+        (accumulator, num) => accumulator *= num,
+        1,
+    );
+
+    return largestNumberSum - smallestNumberSum;
+};

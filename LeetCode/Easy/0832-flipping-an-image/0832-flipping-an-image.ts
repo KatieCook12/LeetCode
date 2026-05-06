1f1e51c0a819b7f9253d6a61016fd486ca11e9f8
Katie <97467617+KatieCook12@users.noreply.github.com>
2026-05-06 13:36:46 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/0832-flipping-an-image/0832-flipping-an-image.ts
+++ LeetCode/Easy/0832-flipping-an-image/0832-flipping-an-image.ts
@@ -1,9 +1,7 @@
 function flipAndInvertImage(image: number[][]): number[][] {
 
     for (const matrix of image) {
-
         const matrixLength: number = matrix.length;
-
         for (let x = 0; x < matrixLength; x++) {
             matrix[x] === 1 ? matrix[x] = 0 : matrix[x] = 1;
         }

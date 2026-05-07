b3c88eb39581c694963f1e145b5ee11a720000e3
Katie <97467617+KatieCook12@users.noreply.github.com>
2026-05-07 10:43:05 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/2974-minimum-number-game/2974-minimum-number-game.ts
+++ LeetCode/Easy/2974-minimum-number-game/2974-minimum-number-game.ts
@@ -1,8 +1,9 @@
 function numberGame(nums: number[]): number[] {
 
     nums.sort((a, b) => a - b);
+    const numsLength: number = nums.length;
 
-    for (let i = 0; i < nums.length; i += 2) {
+    for (let i = 0; i < numsLength; i += 2) {
         [nums[i], nums[i + 1]] = [nums[i + 1], nums[i]];
     }
 

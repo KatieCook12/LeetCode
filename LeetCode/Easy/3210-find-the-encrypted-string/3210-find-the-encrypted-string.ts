446d506bd2f2ab719ae639a7b23d74ba30a50df1
Katie <97467617+KatieCook12@users.noreply.github.com>
2026-05-21 22:29:58 +0100

    Time: 0 ms (100%), Space: 58 MB (45.45%) - LeetHub

--- LeetCode/Easy/3210-find-the-encrypted-string/3210-find-the-encrypted-string.ts
+++ LeetCode/Easy/3210-find-the-encrypted-string/3210-find-the-encrypted-string.ts
@@ -1,11 +1,11 @@
 function getEncryptedString(s: string, k: number): string {
 
-    let encryptedString: string = "";
+    const encryptedString: string[] = [];
     const sLength: number = s.length;
 
     for (let i = 0; i < sLength; i++) {
-        encryptedString += s[(i + k) % sLength];
+        encryptedString.push(s[(i + k) % sLength]);
     }
 
-    return encryptedString;
+    return encryptedString.join("");
 };

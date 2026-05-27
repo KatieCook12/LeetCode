d79d953586893a0033196aa0b72567979ef97e65
Katie <97467617+KatieCook12@users.noreply.github.com>
2026-05-28 00:14:32 +0100

    Time: 1 ms (73.47%), Space: 58.7 MB (11.22%) - LeetHub

--- LeetCode/Easy/3931-check-adjacent-digit-differences/3931-check-adjacent-digit-differences.ts
+++ LeetCode/Easy/3931-check-adjacent-digit-differences/3931-check-adjacent-digit-differences.ts
@@ -3,7 +3,7 @@ function isAdjacentDiffAtMostTwo(s: string): boolean {
     const sLength: number = s.length - 1;
 
     for (let i = 0; i < sLength; i++) {
-        if (Math.abs(+s[i] - +s[i + 1]) > 2) {
+        if (Math.abs(Number(s[i]) - Number(s[i + 1])) > 2) {
             return false;
         }
     }

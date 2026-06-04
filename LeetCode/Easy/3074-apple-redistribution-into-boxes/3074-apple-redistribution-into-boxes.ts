b2f29616358b1978b030f9bd71448e85727d02d8
Katie <97467617+KatieCook12@users.noreply.github.com>
2026-06-04 11:18:28 +0100

    Time: 54 ms (8.2%), Space: 62.2 MB (8.2%) - LeetHub

---
+++ LeetCode/Easy/3074-apple-redistribution-into-boxes/3074-apple-redistribution-into-boxes.ts
@@ -0,0 +1,15 @@
+function minimumBoxes(apple: number[], capacity: number[]): number {
+
+    let appleSum: number = apple.reduce((accumulator, currentValue) => accumulator + currentValue, 0,);
+
+    capacity.sort((a, b) => b - a);
+
+    return capacity.reduce((accumulator, currentValue) => {
+        console.log(appleSum)
+        if (appleSum > 0) {
+            appleSum -= currentValue;
+            accumulator++;
+        }
+        return accumulator;
+    }, 0);
+};

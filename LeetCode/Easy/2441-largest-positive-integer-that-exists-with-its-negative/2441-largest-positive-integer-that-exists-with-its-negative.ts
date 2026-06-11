76c4845c0cdb2cf91cab2946cb3053b7c7979584
Katie <97467617+KatieCook12@users.noreply.github.com>
2026-06-11 15:06:13 +0100

    Time: 4 ms (98.85%), Space: 57.1 MB (92.72%) - LeetHub

--- LeetCode/Easy/2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.ts
+++ LeetCode/Easy/2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.ts
@@ -1,6 +1,6 @@
 function findMaxK(nums: number[]): number {
 
-    const seen = new Set(nums);
+    const seen: Set<number> = new Set(nums);
     const pairs: number[] = [];
 
     for (const num of seen) {
@@ -9,12 +9,5 @@ function findMaxK(nums: number[]): number {
         }
     }
 
-    if (pairs.length === 0){
-        return -1;
-    }
-    else {
-        return Math.max(...pairs);
-    }
-
-
+    return pairs.length === 0 ? -1 : Math.max(...pairs);
 };

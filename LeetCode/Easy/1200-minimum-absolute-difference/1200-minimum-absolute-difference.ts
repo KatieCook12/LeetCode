8aada6579344fd8c43e616dc8ee179594414c137
Katie <97467617+KatieCook12@users.noreply.github.com>
2026-06-21 23:16:21 +0100

    Time: 56 ms (56.99%), Space: 70 MB (87.1%) - LeetHub

--- LeetCode/Easy/1200-minimum-absolute-difference/1200-minimum-absolute-difference.ts
+++ LeetCode/Easy/1200-minimum-absolute-difference/1200-minimum-absolute-difference.ts
@@ -2,22 +2,21 @@ function minimumAbsDifference(arr: number[]): number[][] {
 
     arr.sort((a, b) => a - b);
 
-    let minimumDifference: number = Infinity;
-    const minimumAbsoluteDifferencePairs: number[][] = [];
+    let minimumDifference = Infinity;
+    const pairs: number[][] = [];
     const arrLength: number = arr.length - 1;
 
     for (let i = 0; i < arrLength; i++) {
-        const distance: number = Math.abs(arr[i + 1] - arr[i]);
+        const distance = arr[i + 1] - arr[i];
+
         if (distance < minimumDifference) {
             minimumDifference = distance;
+            pairs.length = 0;
+            pairs.push([arr[i], arr[i + 1]]);
+        } else if (distance === minimumDifference) {
+            pairs.push([arr[i], arr[i + 1]]);
         }
     }
 
-    for (let i = 0; i < arrLength; i++) {
-        if (Math.abs(arr[i + 1] - arr[i]) === minimumDifference) {
-            minimumAbsoluteDifferencePairs.push([arr[i], arr[i + 1]])
-        }
-    }
-
-    return minimumAbsoluteDifferencePairs;
-};
+    return pairs;
+}

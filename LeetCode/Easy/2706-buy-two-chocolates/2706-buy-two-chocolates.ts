111ec07a754bf12c848038083eee8bbe90a583b3
Katie <97467617+KatieCook12@users.noreply.github.com>
2026-06-22 12:01:09 +0100

    Time: 3 ms (59.26%), Space: 60.3 MB (11.11%) - LeetHub

--- LeetCode/Easy/2706-buy-two-chocolates/2706-buy-two-chocolates.ts
+++ LeetCode/Easy/2706-buy-two-chocolates/2706-buy-two-chocolates.ts
@@ -2,11 +2,7 @@ function buyChoco(prices: number[], money: number): number {
 
     prices.sort((a, b) => a - b);
 
-    if (prices[0] + prices[1] <= money) {
-        return money - (prices[0] + prices[1]);
-    }
-    else {
-        return money;
-    }
+    const total: number = prices[0] + prices[1];
 
+    return total <= money ? money - total : money;
 };

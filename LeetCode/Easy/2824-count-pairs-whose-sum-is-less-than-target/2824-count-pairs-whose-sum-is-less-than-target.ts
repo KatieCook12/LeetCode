c700a7bb0b8d44bc480fa654bf1e4b2df57b7488
Katie <97467617+KatieCook12@users.noreply.github.com>
2026-06-26 10:46:40 +0100

    Time: 1 ms (65.96%), Space: 55.8 MB (100%) - LeetHub

---
+++ LeetCode/Easy/2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.ts
@@ -0,0 +1,14 @@
+function countPairs(nums: number[], target: number): number {
+
+    nums.sort((a, b) => a - b)
+    let count: number = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] < target) {
+                count++;
+            }
+        }
+    }
+    return count;
+};
